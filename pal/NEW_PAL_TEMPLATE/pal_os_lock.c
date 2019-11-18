9041a2595ec0059bc4de8a9811ad7b2d88a082e5
Artem Yushev <12692378+ayushev@users.noreply.github.com>
2019-11-18 16:54:48 +0100

    Update pal_os_lock.c

--- pal/NEW_PAL_TEMPLATE/pal_os_lock.c
+++ pal/NEW_PAL_TEMPLATE/pal_os_lock.c
@@ -54,8 +54,8 @@ pal_status_t pal_os_lock_acquire(pal_os_lock_t * p_lock)
 {
     pal_status_t return_status = PAL_STATUS_FAILURE;
 
-	// Below is a sample shared resource acquire mechanism
-	// it doesn't provide a guarantee against a deadlock
+    // Below is a sample shared resource acquire mechanism
+    // it doesn't provide a guarantee against a deadlock
     if (!(p_lock->lock))
     {
         p_lock->lock++;
@@ -70,8 +70,8 @@ pal_status_t pal_os_lock_acquire(pal_os_lock_t * p_lock)
 
 void pal_os_lock_release(pal_os_lock_t * p_lock)
 {
-	// Below is a sample shared resource acquire mechanism
-	// it doesn't provide a guarantee against a deadlock
+    // Below is a sample shared resource acquire mechanism
+    // it doesn't provide a guarantee against a deadlock
     if (0 != p_lock->lock)
     {
         p_lock->lock--;
@@ -80,10 +80,12 @@ void pal_os_lock_release(pal_os_lock_t * p_lock)
 
 void pal_os_lock_enter_critical_section()
 {
+    // For safety critical systems it is recommended to implement a critical section entry
 }
 
 void pal_os_lock_exit_critical_section()
 {
+    // For safety critical systems it is recommended to implement a critical section exit
 }
 
 /**
