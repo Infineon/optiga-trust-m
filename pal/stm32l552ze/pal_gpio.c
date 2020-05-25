b33a5035f765a0fa109bd04ad018ddc9aebf8421
Oscar Sanchez <sanchez@Oscars-MacBook-Pro.local>
2020-05-25 15:25:09 -0700

    addin pal for stm32l552ze

---
+++ pal/stm32l552ze/pal_gpio.c
@@ -0,0 +1,90 @@
+/**
+* \copyright
+* MIT License
+*
+* Copyright (c) 2019 Infineon Technologies AG
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE
+*
+* \endcopyright
+*
+* \author Infineon Technologies AG
+*
+* \file pal_gpio.c
+*
+* \brief   This file implements the platform abstraction layer APIs for GPIO.
+*
+* \ingroup  grPAL
+*
+* @{
+*/
+#include "main.h"
+#include "optiga/pal/pal_gpio.h"
+
+typedef struct stm_gpio_ctx
+{
+	uint32_t		 	p_gpio;
+	GPIO_TypeDef *      p_port;
+}stm_gpio_ctx_t;
+
+//lint --e{714,715} suppress "This is implemented for overall completion of API"
+pal_status_t pal_gpio_init(const pal_gpio_t * p_gpio_context)
+{
+    return PAL_STATUS_SUCCESS;
+}
+
+//lint --e{714,715} suppress "This is implemented for overall completion of API"
+pal_status_t pal_gpio_deinit(const pal_gpio_t * p_gpio_context)
+{
+
+  return PAL_STATUS_SUCCESS;
+}
+
+void pal_gpio_set_high(const pal_gpio_t * p_gpio_context)
+{
+	stm_gpio_ctx_t* p_io = NULL;
+
+    if ((p_gpio_context != NULL) && (p_gpio_context->p_gpio_hw != NULL))
+    {
+		// !!!OPTIGA_LIB_PORTING_REQUIRED
+        // You function to set the pin high
+    	p_io = (stm_gpio_ctx_t*)(p_gpio_context->p_gpio_hw);
+
+    	HAL_GPIO_WritePin(p_io->p_port, p_io->p_gpio, GPIO_PIN_SET);
+
+    }
+}
+
+void pal_gpio_set_low(const pal_gpio_t * p_gpio_context)
+{
+	stm_gpio_ctx_t* p_io = NULL;
+
+    if ((p_gpio_context != NULL) && (p_gpio_context->p_gpio_hw != NULL))
+    {
+    	// !!!OPTIGA_LIB_PORTING_REQUIRED
+		// You function to set the pin high
+		p_io = (stm_gpio_ctx_t*)(p_gpio_context->p_gpio_hw);
+
+		HAL_GPIO_WritePin(p_io->p_port, p_io->p_gpio, GPIO_PIN_RESET);
+    }
+}
+
+/**
+* @}
+*/
---
+++ pal/stm32l552ze/pal_logger.c
@@ -0,0 +1,135 @@
+/**
+* \copyright
+* MIT License
+*
+* Copyright (c) 2019 Infineon Technologies AG
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE
+*
+* \endcopyright
+*
+* \author Infineon Technologies AG
+ *
+ * \file    pal_logger.c
+ *
+ * \brief   This file provides the prototypes declarations for pal logger.
+ *
+ * \ingroup grPAL
+ *
+ * @{
+ */
+#include "main.h"
+#include "optiga/pal/pal_logger.h"
+
+extern UART_HandleTypeDef hlpuart1;
+
+pal_logger_t logger_console =
+{
+	.logger_config_ptr = (void*)(&hlpuart1),
+	.logger_rx_flag = 1,
+	.logger_tx_flag = 1
+};
+
+
+
+pal_status_t pal_logger_init(void * p_logger_context)
+{
+    pal_status_t return_status = PAL_STATUS_FAILURE;
+    pal_logger_t * p_log_context = p_logger_context;
+
+    do
+    {
+    	return_status = PAL_STATUS_SUCCESS;
+    }while(FALSE);
+    return return_status;
+}
+
+
+pal_status_t pal_logger_deinit(void * p_logger_context)
+{
+    pal_status_t return_status = PAL_STATUS_FAILURE;
+    pal_logger_t * p_log_context = p_logger_context;
+
+    do
+    {
+		// !!!OPTIGA_LIB_PORTING_REQUIRED
+    }while(FALSE);
+    return return_status;
+}
+
+
+pal_status_t pal_logger_write(void * p_logger_context, const uint8_t * p_log_data, uint32_t log_data_length)
+{
+
+	HAL_StatusTypeDef return_status = HAL_ERROR;
+    pal_logger_t * p_log_context = p_logger_context;
+
+    do{
+    	pal_logger_t * p_log_context = p_logger_context;
+
+		p_log_context->logger_tx_flag = 1U;
+
+		HAL_UART_Transmit(p_log_context->logger_config_ptr, (uint8_t *)p_log_data, log_data_length, 0xffff);
+		if (return_status != PAL_STATUS_SUCCESS)
+		{
+			break;
+		}
+		while (p_log_context->logger_tx_flag)
+		{
+
+		}
+		return_status = PAL_STATUS_SUCCESS;
+
+    }while(0);
+
+    return ((pal_status_t)return_status);
+}
+
+pal_status_t pal_logger_read(void * p_logger_context, uint8_t * p_log_data, uint32_t log_data_length)
+{
+
+	HAL_StatusTypeDef return_status = HAL_ERROR;
+	pal_logger_t * p_log_context = p_logger_context;
+
+	do{
+		int32_t return_status = PAL_STATUS_FAILURE;
+		pal_logger_t * p_log_context = p_logger_context;
+
+		p_log_context->logger_rx_flag = 1U;
+		HAL_UART_Receive(p_log_context->logger_config_ptr, p_log_data, log_data_length, 0xffff);
+		if (return_status != PAL_STATUS_SUCCESS)
+		{
+			break;
+		}
+		while (p_log_context->logger_rx_flag)
+		{
+
+		}
+		return_status = PAL_STATUS_SUCCESS;
+
+	}while(0);
+    return ((pal_status_t)return_status);
+}
+
+
+
+
+/**
+ * @}
+ */
