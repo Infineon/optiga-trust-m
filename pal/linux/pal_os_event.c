4c342cdb80f3c24ca79ccfcf2586742cca60c5ad
Ying <ying.ye@infineon.com>
2020-10-13 11:24:39 +0800

    1)Modify pal_os_datastore.c to resolve secure shield communication issue for new trustm_lib 2)Modify pal_os_event.c to resolve runtime segmentation fault of openssl engine usage by adding ARM and DISARM functions 3)Set default reset to Soft Reset, do not use external HW reset pin

--- pal/linux/pal_os_event.c
+++ pal/linux/pal_os_event.c
@@ -2,7 +2,7 @@
 * \copyright
 * MIT License
 *
-* Copyright (c) 2019 Infineon Technologies AG
+* Copyright (c) 2020 Infineon Technologies AG
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -39,7 +39,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
-#include <errno.h>
 #include <time.h>
 #include "optiga/pal/pal_os_timer.h"
 #include "optiga/pal/pal_os_event.h"
@@ -68,48 +67,97 @@
 
 static void handler(int sig, siginfo_t *si, void *uc)
 {
-    TRUSTM_PAL_EVENT_DBGFN(">"); 
-    pal_os_event_trigger_registered_callback();
-    TRUSTM_PAL_EVENT_DBGFN("<");   
+	TRUSTM_PAL_EVENT_DBGFN(">");    
+	pal_os_event_trigger_registered_callback();
+	TRUSTM_PAL_EVENT_DBGFN("<");    
 }
 
 
 /// @cond hidden
 
 static pal_os_event_t pal_os_event_0 = {0};
-static     timer_t timerid;
+static 	timer_t timerid;
 
 void pal_os_event_start(pal_os_event_t * p_pal_os_event, register_callback callback, void * callback_args)
 {
-    TRUSTM_PAL_EVENT_DBGFN(">");
-
+    TRUSTM_PAL_EVENT_DBGFN(">");    
+    
     if(FALSE == p_pal_os_event->is_event_triggered)
     {
         p_pal_os_event->is_event_triggered = TRUE;
         pal_os_event_register_callback_oneshot(p_pal_os_event,callback,callback_args,1000);
     }
+    TRUSTM_PAL_EVENT_DBGFN("<");    
 
-    TRUSTM_PAL_EVENT_DBGFN("<"); 
 }
 
 void pal_os_event_stop(pal_os_event_t * p_pal_os_event)
 {
-    TRUSTM_PAL_EVENT_DBGFN(">");
+    TRUSTM_PAL_EVENT_DBGFN(">");    
+    
     //lint --e{714} suppress "The API pal_os_event_stop is not exposed in header file but used as extern in optiga_cmd.c"
     p_pal_os_event->is_event_triggered = FALSE;
-    TRUSTM_PAL_EVENT_DBGFN("<"); 
+
+    TRUSTM_PAL_EVENT_DBGFN("<");    
+
+}
+
+void pal_os_event_disarm(void)
+{
+	struct itimerspec its;
+
+	TRUSTM_PAL_EVENT_DBGFN(">");    
+	its.it_value.tv_sec = 0;
+	its.it_value.tv_nsec = 0;
+	its.it_interval.tv_sec = 0;
+	its.it_interval.tv_nsec = 0;
+	
+	if (timer_settime(timerid, 0, &its, NULL) == -1)
+	{
+		printf("Error in timer_settime\n");
+	    exit(1);
+	}
+
+	TRUSTM_PAL_EVENT_DBGFN("<");    
+} 
+
+void pal_os_event_arm(void)
+{   
+	struct itimerspec its;
+
+	TRUSTM_PAL_EVENT_DBGFN(">");    
+	its.it_value.tv_sec = 0;
+	its.it_value.tv_nsec = 1000000;
+	its.it_interval.tv_sec = 0;
+	its.it_interval.tv_nsec = 294967296;
+	
+	if (timer_settime(timerid, 0, &its, NULL) == -1)
+	{
+		printf("Error in timer_settime\n");
+	    exit(1);
+	}
+
+	TRUSTM_PAL_EVENT_DBGFN("<");    
+} 
+
+void pal_os_event_destroy1(void)
+{
+    TRUSTM_PAL_EVENT_DBGFN(">");    
+    timer_delete(timerid);
+    TRUSTM_PAL_EVENT_DBGFN("<");    
 }
 
 pal_os_event_t * pal_os_event_create(register_callback callback, void * callback_args)
 {
     struct sigevent sev;
     struct sigaction sa;
-    
-    TRUSTM_PAL_EVENT_DBGFN(">");
-    
+
+    TRUSTM_PAL_EVENT_DBGFN(">");    
+	
     if(( NULL != callback )&&( NULL != callback_args ))
     {
-        /* Establishing handler for signal */
+		/* Establishing handler for signal */
+		
         sa.sa_flags = SA_SIGINFO;
         sa.sa_sigaction = handler;
         sigemptyset(&sa.sa_mask);
@@ -126,14 +174,14 @@ pal_os_event_t * pal_os_event_create(register_callback callback, void * callback
         sev.sigev_value.sival_ptr = &timerid;
         if (timer_create(CLOCKID, &sev, &timerid) == -1)
         {
-            printf("error in timer_create\n");
+            printf("timer_create\n");
             exit(1);
         }
 
         pal_os_event_start(&pal_os_event_0,callback,callback_args);
     }
-    
-    TRUSTM_PAL_EVENT_DBGFN("<");
+
+    TRUSTM_PAL_EVENT_DBGFN("<");    
     
     return (&pal_os_event_0);
 }
@@ -143,27 +191,33 @@ void pal_os_event_trigger_registered_callback(void)
     register_callback callback;
     struct itimerspec its;
 
-    TRUSTM_PAL_EVENT_DBGFN(">");  
-    
-    its.it_value.tv_sec = 0;
-    its.it_value.tv_nsec = 0;
-    its.it_interval.tv_sec = 0;
-    its.it_interval.tv_nsec = 0;
-    
-    if (timer_settime(timerid, 0, &its, NULL) == -1)
-    {
-        fprintf(stderr, "Error in timer_settime\n");
-        exit(1);
-    }
+    // !!!OPTIGA_LIB_PORTING_REQUIRED
+    // The following steps related to TIMER must be taken care while porting to different platform
+    // TBD
+    TRUSTM_PAL_EVENT_DBGFN(">");    
 
     if (pal_os_event_0.callback_registered)
     {
         callback = pal_os_event_0.callback_registered;
         pal_os_event_0.callback_registered = NULL;
+	
+	// Stop the timer
+	its.it_value.tv_sec = 0;
+	its.it_value.tv_nsec = 0;
+	its.it_interval.tv_sec = 0;
+	its.it_interval.tv_nsec = 0;
+
+	if (timer_settime(timerid, 0, &its, NULL) == -1)
+	{
+	    TRUSTM_PAL_EVENT_ERRFN("Fail to stop the timer\n");
+	    exit(1);
+	}
+	
         callback((void * )pal_os_event_0.callback_ctx);
     }
-
-    TRUSTM_PAL_EVENT_DBGFN("<"); 
+    
+    TRUSTM_PAL_EVENT_DBGFN("<");    
+    
 }
 /// @endcond
 
@@ -172,52 +226,47 @@ void pal_os_event_register_callback_oneshot(pal_os_event_t * p_pal_os_event,
                                              void * callback_args,
                                              uint32_t time_us)
 {
-    struct itimerspec its;
-    long long freq_nanosecs;
-    int ret = 0;
-    //sigset_t mask;
+	struct itimerspec its;
+	long long freq_nanosecs;
+	//sigset_t mask;
 
-    TRUSTM_PAL_EVENT_DBGFN(">");
-    
-    //uint8_t scheduler_timer;
+	TRUSTM_PAL_EVENT_DBGFN(">");    
+
+
+	//uint8_t scheduler_timer;
     p_pal_os_event->callback_registered = callback;
     p_pal_os_event->callback_ctx = callback_args;
-    
-    /* Start the timer */
-    freq_nanosecs = time_us * 1000;
-    its.it_value.tv_sec = (freq_nanosecs / 1000000000);
-    its.it_value.tv_nsec = (freq_nanosecs % 1000000000);
-    its.it_interval.tv_sec = 0;
-    its.it_interval.tv_nsec = 0;
-    
-    if (( ret = timer_settime(timerid, 0, &its, NULL)) == -1)    
-    {
-        int errsv = errno;
-        fprintf(stderr,"timer_settime FAILED!!!\n");
-        if(errsv == EINVAL)
-        {
-            fprintf(stderr,"INVALID VALUE!\n");
-        }
-        else
-        {
-            fprintf(stderr,"UNKOWN ERROR: %d\n",errsv);
-        }
-        exit(1);
-    }
-    
-    TRUSTM_PAL_EVENT_DBGFN("<"); 
+	
+	/* Start the timer */
+
+	freq_nanosecs = time_us * 1000;
+	its.it_value.tv_sec = freq_nanosecs / 1000000000;
+	its.it_value.tv_nsec = freq_nanosecs % 1000000000;
+	its.it_interval.tv_sec = its.it_value.tv_sec;
+	its.it_interval.tv_nsec = its.it_value.tv_nsec;
+
+	//printf("freq_nanosecs = %lld\n", time_us * 1000);
+	//printf("its.it_value.tv_sec = %lld", freq_nanosecs / 1000000000);
+	//printf("its.it_value.tv_nsec = %lld\n", freq_nanosecs % 1000000000);
+	//printf("its.it_interval.tv_sec = %lld\n", its.it_value.tv_sec);
+	//printf("its.it_interval.tv_nsec = %lld\n", its.it_value.tv_nsec);
+
+	
+	if (timer_settime(timerid, 0, &its, NULL) == -1)
+	{
+		printf("timer_settime\n");
+	    exit(1);
+	}
+	TRUSTM_PAL_EVENT_DBGFN("<");    
 }
 
 //lint --e{818,715} suppress "As there is no implementation, pal_os_event is not used"
 void pal_os_event_destroy(pal_os_event_t * pal_os_event)
 {
-    TRUSTM_PAL_EVENT_DBGFN(">"); 
-    pal_os_event_stop(pal_os_event);
-    if (timerid != 0)
-    {
-        timer_delete(timerid);
-    }
-    TRUSTM_PAL_EVENT_DBGFN("<"); 
+    TRUSTM_PAL_EVENT_DBGFN(">");    
+    timer_delete(timerid);
+    TRUSTM_PAL_EVENT_DBGFN("<");    
+
 }
 
 /**
